78cc9ed4596c7d8e30bf4652ea06f3750bc5d31d
Fabio Maulo <fabiomaulo@gmail.com = Fabio Maulo fabiomaulo@gmail.com>
2008-04-28 22:51:42 +0000

    Test for NotNullOrEmptyValidator (removed logging for performance)

---
+++ src/NHibernate.Validator.Tests/ValidatorsTest/NotNullOrEmptyValidatorFixture.cs
@@ -0,0 +1,24 @@
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace NHibernate.Validator.Tests.ValidatorsTest
+{
+	[TestFixture]
+	public class NotNullOrEmptyValidatorFixture
+	{
+		[Test]
+		public void IsValid()
+		{
+			NotNullOrEmptyValidator v = new NotNullOrEmptyValidator();
+			Assert.IsTrue(v.IsValid("abc"));
+			Assert.IsTrue(v.IsValid(new int[] {1}));
+			Assert.IsTrue(v.IsValid(new List<int>(new int[] { 1 })));
+
+			Assert.IsFalse(v.IsValid(null));
+			Assert.IsFalse(v.IsValid(""));
+			Assert.IsFalse(v.IsValid(123));
+			Assert.IsFalse(v.IsValid(new int[0]));
+			Assert.IsFalse(v.IsValid(new List<int>()));
+		}
+	}
+}
--- src/NHibernate.Validator/NullOrEmptyValidator.cs
+++ src/NHibernate.Validator/NullOrEmptyValidator.cs
@@ -1,25 +1,22 @@
-using System;
 using System.Collections;
-using log4net;
 using NHibernate.Validator.Engine;
 
 namespace NHibernate.Validator
 {
+	/// <summary>
+	/// Validator for any kind of IEnumerable (including string)
+	/// </summary>
 	public class NotNullOrEmptyValidator : IValidator
 	{
-		private static readonly ILog log = LogManager.GetLogger(typeof(NotNullOrEmptyValidator));
 		public bool IsValid(object value)
 		{
-			log.Info(value == null ? "null" : value.ToString());
-			if (value == null) return false;
+			IEnumerable ev = value as IEnumerable;
+			if (ev != null)
+			{
+				return ev.GetEnumerator().MoveNext();
+			}
 
-			if (value is ICollection)
-				return ((ICollection)value).Count > 0;
-
-			if (value is string)
-				return ((string)value).Length > 0;
-
-			throw new ArgumentException("the object to validate must be a string or a collection", "value");
+			return false;
 		}
 	}
 }
