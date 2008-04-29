783c5603d881a9a7dc49de0c5a58207c86869f32
Fabio Maulo <fabiomaulo@gmail.com = Fabio Maulo fabiomaulo@gmail.com>
2008-04-29 06:30:36 +0000

    Improving Tests for RuleAttributeFactory

--- src/NHibernate.Validator.Tests/Configuration/RuleAttributeFactoryFixture.cs
+++ src/NHibernate.Validator.Tests/Configuration/RuleAttributeFactoryFixture.cs
@@ -48,13 +48,80 @@ namespace NHibernate.Validator.Tests.Configuration
 			NhvmClass cm = map.@class[0];
 			XmlClassMapping rm = new XmlClassMapping(cm);
 			MemberInfo mi;
-			List<Attribute> attr;
+			List<Attribute> attributes;
 
 			mi = typeof(WellKnownRules).GetField("AP");
-			attr = new List<Attribute>(rm.GetMemberAttributes(mi));
-			Assert.AreEqual("A string value", ((ACustomAttribute) attr[0]).Value1);
-			Assert.AreEqual(123, ((ACustomAttribute)attr[0]).Value2);
-			Assert.AreEqual("custom message", ((ACustomAttribute)attr[0]).Message);
+			attributes = new List<Attribute>(rm.GetMemberAttributes(mi));
+			Assert.AreEqual("A string value", ((ACustomAttribute)attributes[0]).Value1);
+			Assert.AreEqual(123, ((ACustomAttribute)attributes[0]).Value2);
+			Assert.AreEqual("custom message", ((ACustomAttribute)attributes[0]).Message);
+
+			mi = typeof(WellKnownRules).GetField("StrProp");
+			attributes = new List<Attribute>(rm.GetMemberAttributes(mi));
+			NotEmptyAttribute nea = FindAttribute<NotEmptyAttribute>(attributes);
+			Assert.AreEqual("not-empty message", nea.Message);
+			
+			NotNullAttribute nna = FindAttribute<NotNullAttribute>(attributes);
+			Assert.AreEqual("not-null message", nna.Message);
+			
+			NotNullOrEmptyAttribute nnea = FindAttribute<NotNullOrEmptyAttribute>(attributes);
+			Assert.AreEqual("notnullorempty message", nnea.Message);
+			
+			LengthAttribute la = FindAttribute<LengthAttribute>(attributes);
+			Assert.AreEqual("length message", la.Message);
+			Assert.AreEqual(1, la.Min);
+			Assert.AreEqual(10, la.Max);
+			
+			PatternAttribute pa = FindAttribute<PatternAttribute>(attributes);
+			Assert.AreEqual("pattern message", pa.Message);
+			Assert.AreEqual("[0-9]+", pa.Regex);
+			// TODO : Change the XSD to allow the definition of PatternAttribute.RegexOptions
+
+			EmailAttribute ea = FindAttribute<EmailAttribute>(attributes);
+			Assert.AreEqual("email message", ea.Message);
+
+			IPAddressAttribute ipa = FindAttribute<IPAddressAttribute>(attributes);
+			Assert.AreEqual("ipAddress message", ipa.Message);
+
+			mi = typeof(WellKnownRules).GetField("DtProp");
+			attributes = new List<Attribute>(rm.GetMemberAttributes(mi));
+			FutureAttribute fa = FindAttribute<FutureAttribute>(attributes);
+			Assert.AreEqual("future message", fa.Message);
+			PastAttribute psa = FindAttribute<PastAttribute>(attributes);
+			Assert.AreEqual("past message", psa.Message);
+
+			mi = typeof(WellKnownRules).GetField("DecProp");
+			attributes = new List<Attribute>(rm.GetMemberAttributes(mi));
+			DigitsAttribute dga = FindAttribute<DigitsAttribute>(attributes);
+			Assert.AreEqual("digits message", dga.Message);
+			Assert.AreEqual(5, dga.IntegerDigits);
+			Assert.AreEqual(2, dga.FractionalDigits);
+
+			MinAttribute mina = FindAttribute<MinAttribute>(attributes);
+			Assert.AreEqual("min message", mina.Message);
+			Assert.AreEqual(100, mina.Value);
+
+			MaxAttribute maxa = FindAttribute<MaxAttribute>(attributes);
+			Assert.AreEqual("max message", maxa.Message);
+			Assert.AreEqual(200, maxa.Value);
+
+			mi = typeof(WellKnownRules).GetField("BProp");
+			attributes = new List<Attribute>(rm.GetMemberAttributes(mi));
+			AssertTrueAttribute ata = FindAttribute<AssertTrueAttribute>(attributes);
+			Assert.AreEqual("asserttrue message", ata.Message);
+
+			mi = typeof(WellKnownRules).GetField("ArrProp");
+			attributes = new List<Attribute>(rm.GetMemberAttributes(mi));
+			SizeAttribute sa = FindAttribute<SizeAttribute>(attributes);
+			Assert.AreEqual("size message", sa.Message);
+			Assert.AreEqual(2, sa.Min);
+			Assert.AreEqual(9, sa.Max);
+		}
+
+		private static T FindAttribute<T>(List<Attribute> attr) where T : Attribute
+		{
+			return (T)attr.Find(delegate(Attribute a)
+														{ return a is T; });
 		}
 	}
 }
--- src/NHibernate.Validator.Tests/Configuration/WellKnownRules.cs
+++ src/NHibernate.Validator.Tests/Configuration/WellKnownRules.cs
@@ -1,8 +1,14 @@
+using System;
+
 namespace NHibernate.Validator.Tests.Configuration
 {
 	public class WellKnownRules
 	{
 		public string AP;
 		public string StrProp;
+		public DateTime DtProp;
+		public decimal DecProp;
+		public bool BProp;
+		public int[] ArrProp;
 	}
 }
