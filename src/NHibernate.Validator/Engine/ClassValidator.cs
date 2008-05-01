3e3daacd2eb85e0dc9d6752a453686d2ef5cabf7
Fabio Maulo <fabiomaulo@gmail.com = Fabio Maulo fabiomaulo@gmail.com>
2008-05-01 04:41:28 +0000

    Unified bad formed validator catching exception

--- src/NHibernate.Validator/Engine/ClassValidator.cs
+++ src/NHibernate.Validator/Engine/ClassValidator.cs
@@ -493,13 +493,9 @@ namespace NHibernate.Validator.Engine
 				defaultInterpolator.AddInterpolator(attribute, beanValidator);
 				return beanValidator;
 			}
-			catch (HibernateValidatorException ex)
-			{
-				throw new HibernateValidatorException("could not instantiate ClassValidator, maybe some validator is not well formed", ex);
-			}
 			catch (Exception ex)
 			{
-				throw new HibernateValidatorException("could not instantiate ClassValidator", ex);
+				throw new HibernateValidatorException("could not instantiate ClassValidator, maybe some validator is not well formed; check InnerException", ex);
 			}
 		}
 
