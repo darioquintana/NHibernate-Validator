f60ffa49a3ab5fdc3ea2e7c62878063d88b11377
Dario Quintana <contacto@darioquintana.com.ar = Dario Quintana contacto@darioquintana.com.ar>
2008-08-06 00:25:59 +0000

    going to v1.0 CR1

--- src/NHibernate.Validator.Specific/AssemblyInfo.cs
+++ src/NHibernate.Validator.Specific/AssemblyInfo.cs
@@ -8,6 +8,6 @@ using System.Reflection;
 [assembly: AssemblyProduct("NHibernate.Validator")]
 [assembly: AssemblyCopyright("Licensed under LGPL.")]
 [assembly: AssemblyVersionAttribute("1.0.0.0")]
-[assembly: AssemblyInformationalVersionAttribute("1.0.0.0 Beta1")]
-[assembly: AssemblyFileVersionAttribute("1.0.0.0 Beta1")]
+[assembly: AssemblyInformationalVersionAttribute("1.0.0.0 CR1")]
+[assembly: AssemblyFileVersionAttribute("1.0.0.0 CR1")]
 [assembly: AssemblyDelaySignAttribute(false)]
