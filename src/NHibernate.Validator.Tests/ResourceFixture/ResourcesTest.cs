a40fe5d0ae2c84ea8fc83194e8b85f80c7f2950d
Fabio Maulo <fabiomaulo@gmail.com = Fabio Maulo fabiomaulo@gmail.com>
2009-02-10 15:46:07 +0000

    Fix NHV-30 (thanks to #ish)

--- src/NHibernate.Validator.Tests/ResourceFixture/ResourcesTest.cs
+++ src/NHibernate.Validator.Tests/ResourceFixture/ResourcesTest.cs
@@ -12,13 +12,23 @@ namespace NHibernate.Validator.Tests.ResourceFixture
 		[Test]
 		public void CanGetResources()
 		{
-			ResourceManager rm = new ResourceManager(Environment.BaseNameOfMessageResource, typeof (IClassValidator).Assembly);
+			var rm = new ResourceManager(Environment.BaseNameOfMessageResource, typeof (IClassValidator).Assembly);
 			Assert.IsNotNull(rm.GetString("validator.length", CultureInfo.InvariantCulture));
 
+			string s_en = rm.GetString("validator.length", CultureInfo.InvariantCulture);
+			string s_de = rm.GetString("validator.length", new CultureInfo("de"));
 			string s_es = rm.GetString("validator.length", new CultureInfo("es"));
+			string s_fr = rm.GetString("validator.length", new CultureInfo("fr"));
 			string s_it = rm.GetString("validator.length", new CultureInfo("it"));
+			string s_lv = rm.GetString("validator.length", new CultureInfo("lv"));
+			string s_nl = rm.GetString("validator.length", new CultureInfo("nl"));
 
-			Assert.AreNotEqual(s_es, s_it);
+			Assert.AreNotEqual(s_en, s_de);
+			Assert.AreNotEqual(s_en, s_es);
+			Assert.AreNotEqual(s_en, s_fr);
+			Assert.AreNotEqual(s_en, s_it);
+			Assert.AreNotEqual(s_en, s_lv);
+			Assert.AreNotEqual(s_en, s_nl);
 		}
 	}
 }
