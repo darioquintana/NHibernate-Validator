5d54ba7c2e45bcc3dc16f787a6583fe971d25a63
Dario Quintana <contacto@darioquintana.com.ar = Dario Quintana contacto@darioquintana.com.ar>
2010-08-25 22:40:16 +0000

    test for NHV95, not fixed yet

---
+++ src/NHibernate.Validator.Tests/Specifics/NHV95/FixtureNhv95.cs
@@ -0,0 +1,42 @@
+﻿using System.Linq;
+using NHibernate.Validator.Cfg.Loquacious;
+using NHibernate.Validator.Engine;
+using NUnit.Framework;
+using SharpTestsEx;
+
+namespace NHibernate.Validator.Tests.Specifics.NHV95
+{
+	internal class FixtureNhv95
+	{
+		[Test,Ignore("Not fixed yet")]
+		public void ValidateOnBaseClasses()
+		{
+			var configure = new FluentConfiguration();
+			configure.Register(new[] {typeof (BaseEntityValidationDef)})
+				.SetDefaultValidatorMode(ValidatorMode.UseExternal);
+
+			var ve = new ValidatorEngine();
+			ve.Configure(configure);
+
+			ve.Validate(new Entity()).Any().Should().Be.True();
+			//ve.GetClassValidator(typeof (Entity)).GetInvalidValues(new Entity()).Any().Should().Be.True();
+		}
+
+		public class BaseEntity
+		{
+			public string StringValue { get; set; }
+		}
+
+		public class BaseEntityValidationDef : ValidationDef<BaseEntity>
+		{
+			public BaseEntityValidationDef()
+			{
+				Define(x => x.StringValue).NotNullableAndNotEmpty();
+			}
+		}
+
+		public class Entity : BaseEntity
+		{
+		}
+	}
+}
