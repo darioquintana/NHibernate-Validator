0af875b6548ac3c21222a23eee062922b0e241b8
Dario Quintana <contacto@darioquintana.com.ar = Dario Quintana contacto@darioquintana.com.ar>
2010-08-25 20:07:53 +0000

    Demo of how to be use the min and max validation

---
+++ src/NHibernate.Validator.Tests/Specifics/NHV98/FixtureNvh98.cs
@@ -0,0 +1,55 @@
+﻿using System.Linq;
+using NHibernate.Validator.Cfg.Loquacious;
+using NHibernate.Validator.Engine;
+using NUnit.Framework;
+using SharpTestsEx;
+
+namespace NHibernate.Validator.Tests.Specifics.NHV98
+{
+	public class FixtureNvh98
+	{
+		[Test]
+		public void MaxLengthWorks()
+		{
+			var configure = new FluentConfiguration();
+			configure.Register(new[] {typeof (EntityValidator)})
+				.SetDefaultValidatorMode(ValidatorMode.UseExternal);
+
+			var entity = new TwoRuleEntity
+			             	{
+			             		SomeString = string.Empty.PadLeft(10).Replace(" ", "A")
+			             	};
+
+			//passes
+			entity.SomeString.Length.Should().Be.GreaterThan(5);
+
+			var ve = new ValidatorEngine();
+			ve.Configure(configure);
+
+			ve.Validate(entity).Any().Should().Be.True();
+		}
+
+		#region Nested type: EntityValidator
+
+		public class EntityValidator : ValidationDef<TwoRuleEntity>
+		{
+			public EntityValidator()
+			{
+				Define(r => r.SomeString)
+					.LengthBetween(1,5)
+					.WithMessage("Error: Can't be less of 1 or over 5 characters long");
+			}
+		}
+
+		#endregion
+
+		#region Nested type: TwoRuleEntity
+
+		public class TwoRuleEntity
+		{
+			public virtual string SomeString { get; set; }
+		}
+
+		#endregion
+	}
+}
