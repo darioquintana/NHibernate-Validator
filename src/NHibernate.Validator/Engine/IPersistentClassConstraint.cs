63e2d9cfc28218c7a0d20eb38da45ef9ea6cc948
Dario Quintana <contacto@darioquintana.com.ar = Dario Quintana contacto@darioquintana.com.ar>
2010-10-13 03:42:51 +0000

    minor

--- src/NHibernate.Validator/Engine/IPersistentClassConstraint.cs
+++ src/NHibernate.Validator/Engine/IPersistentClassConstraint.cs
@@ -5,12 +5,12 @@ namespace NHibernate.Validator.Engine
 	/// <summary>
 	/// Interface implemented by the validator
 	/// when a constraint may be represented in the
-	/// hibernate metadata
+	/// NHibernate metadata
 	/// </summary>
 	public interface IPersistentClassConstraint
 	{
 		/// <summary>
-		///  Apply the constraint in the hibernate metadata
+		///  Apply the constraint in the NHibernate metadata
 		/// </summary>
 		/// <param name="persistentClass">PersistentClass</param>
 		void Apply(PersistentClass persistentClass);
