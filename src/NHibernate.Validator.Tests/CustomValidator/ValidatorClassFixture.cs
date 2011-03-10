89a1df36041938c8d5f3872029c734eb8669b705
Dario Quintana <contacto@darioquintana.com.ar = Dario Quintana contacto@darioquintana.com.ar>
2011-03-10 16:13:35 +0000

    reformatting

--- src/NHibernate.Validator.Tests/CustomValidator/ValidatorClassFixture.cs
+++ src/NHibernate.Validator.Tests/CustomValidator/ValidatorClassFixture.cs
@@ -4,45 +4,40 @@ using NUnit.Framework;
 namespace NHibernate.Validator.Tests.CustomValidator
 {
 	[TestFixture]
-	public class ValidatorClassFixture 
+	public class ValidatorClassFixture
 	{
-		[Test]
-		public void OnlyAttributes()
-		{
-			ValidatorClassAttribute vc1 =
-				new ValidatorClassAttribute(
-					"NHibernate.Validator.Tests.CustomValidator.IsOneValidator, NHibernate.Validator.Tests");
-
-			Assert.IsNotNull(vc1.Value);
-
-			ValidatorClassAttribute vc2 =
-				new ValidatorClassAttribute(typeof(IsOneValidator));
-
-			Assert.IsNotNull(vc1.Value);
-
-			Assert.AreEqual(vc1.Value,vc2.Value);
-
-		}
-
 		[Test]
 		public void IntegrationWithValidation()
 		{
-			ValidatorEngine ve = new ValidatorEngine();
+			var ve = new ValidatorEngine();
 			ve.AssertValid(new Foo(1));
 
 			Assert.IsFalse(ve.IsValid(new Foo(3)));
 		}
-		
+
+		[Test]
+		public void OnlyAttributes()
+		{
+			var vc1 = new ValidatorClassAttribute(
+					"NHibernate.Validator.Tests.CustomValidator.IsOneValidator, NHibernate.Validator.Tests");
+
+			Assert.IsNotNull(vc1.Value);
+
+			var vc2 = new ValidatorClassAttribute(typeof(IsOneValidator));
+
+			Assert.IsNotNull(vc1.Value);
+
+			Assert.AreEqual(vc1.Value, vc2.Value);
+		}
+
 		public class Foo
 		{
+			[IsOne] public int Bar;
+
 			public Foo(int bar)
 			{
 				Bar = bar;
 			}
-
-			[IsOne]
-			public int Bar;
 		}
-
 	}
 }
